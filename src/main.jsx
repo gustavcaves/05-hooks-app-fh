f5074f3a3bf74b16423c8330e1489d2fcfa394a9
gustavcaves <rungustavo@gmail.com>
2023-01-13 20:11:56 +0100

    tarea memo

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { CallBackHook } from './06-memos/CallBackHook'
+// import { MemoHook } from './06-memos/MemoHook'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
@@ -7,12 +9,12 @@ import ReactDOM from 'react-dom/client'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
+// import { Memorize } from './06-memos/Memorize'
 // import { HooksApp } from './HooksApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Memorize />
+    <CallBackHook />
   // </React.StrictMode>
 )
