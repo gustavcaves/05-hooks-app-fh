cd8f229803fa98c4e2ea61489434bc936bcc6a3f
gustavcaves <rungustavo@gmail.com>
2023-01-13 20:11:33 +0100

    useCallback with arguments or not

---
+++ src/06-memos/ShowIncrement.jsx
@@ -0,0 +1,18 @@
+import React, { memo } from "react";
+
+export const ShowIncrement = React.memo( ( { increment }) => {
+
+    console.log('Me volví a generar :(')
+
+  return (
+    <button
+        className="btn btn-primary"
+        onClick={() => {
+            increment( 5 );
+        }}
+    >
+      Incrementar
+    </button>
+  )
+}
+)
