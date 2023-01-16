66ae5ca04a923cf59216766443d27ce8cdc3e5c6
gustavcaves <rungustavo@gmail.com>
2023-01-16 17:14:37 +0100

    practical exercise

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CallBackHook } from './06-memos/CallBackHook'
-import { Padre } from './07-tarea-memo/Padre'
+// import { CallBackHook } from './06-memos/CallBackHook'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -12,10 +11,14 @@ import { Padre } from './07-tarea-memo/Padre'
 // import { Layout } from './05-useLayoutEffect/Layout'
 // import { Memorize } from './06-memos/Memorize'
 // import { HooksApp } from './HooksApp'
+// import { Padre } from './07-tarea-memo/Padre'
+// import './08-useReducer/intro-reducer'
+import { TodoApp } from './08-useReducer/TodoApp'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Padre />
+    <TodoApp />
   // </React.StrictMode>
 )
